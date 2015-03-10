139a3f436961322fa9316d442b5e1d5023f88549
Frank Ortiz <ortiz_frank@hotmail.com>
2015-03-09 22:08:11 -0400

    Creating logic to handle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,43 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static GameManager instance = null; 
+	private GenerationScript generationScript;
+
+	// Consistent player stats
+	public int armor = 0;			// armor carried from previous generation
+	public int weapon = 0;			// weapon carried from previous generation
+	public int exp = 0;				// experience carried from previous generation
+	private int currentGeneration = 0; 
+	public int previousGeneration = 0;  
+	public int totalExp = 0;
+	public int enemiesKilled = 0;
+	public int furthestStage = 0;
+	public int eldestHero = 0;
+	public int mostSkillUpgrades = 0;
+
+
+	void Awake () {
+		if (instance == null)			// Check if instance already exists
+			instance = this;			// if not, set instance to this (GameManager)
+		else if (instance != this)		// If instance already exists and it's not this:
+			Destroy(gameObject);   		// Then destroy this. This enforces our singleton pattern, 
+										// meaning there can only ever be one instance of a GameManager. 
+
+		DontDestroyOnLoad(gameObject);	// Sets this to not be destroyed when reloading scene
+
+		generationScript = GetComponent<GenerationScript>();
+										// Get a component reference to the attached GenerationScript
+
+		InitGame();						// Call the InitGame function to initialize the first level 
 	}
 	
+	void InitGame(){		//Initializes the game for each level.
+
+		generationScript.SetupScene (currentGeneration);	// Call the SetupScene function of the GenerationScript
+															// pass it current generation number.
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
--- Assets/Scripts/GenerationScript.cs
+++ Assets/Scripts/GenerationScript.cs
@@ -1,7 +1,24 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using Random = UnityEngine.Random;
 
 public class GenerationScript : MonoBehaviour {
+
+		
+	[Serializable]						// Using Serializable allows us to embed a class with sub properties in the inspector.
+	public class Count{
+		public int minimum;             //	Minimum value for our Count class.
+		public int maximum;             //	Maximum value for our Count class.
+
+		public Count (int min, int max) //	Assignment constructor.
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+	public GameObject[] enemyUnits;             //	Array of enemy prefabs.
+	public GameObject[] bossUnits;              // Array of outer tile prefabs.
 
 	// Use this for initialization
 	void Start () {
